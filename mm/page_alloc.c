93e9ddc3b030a69e6cf1667ecbf40c3c2c7125c0
Sultan Alsawaf <sultan@kerneltoast.com>
2020-04-19 19:59:18 -0700

    mm: Stop kswapd early when nothing's waiting for it to free pages
    Keeping kswapd running when all the failed allocations that invoked it
    are satisfied incurs a high overhead due to unnecessary page eviction
    and writeback, as well as spurious VM pressure events to various
    registered shrinkers. When kswapd doesn't need to work to make an
    allocation succeed anymore, stop it prematurely to save resources.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: putraaxzy <ptraasw17@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4695,9 +4695,11 @@ __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 	int no_progress_loops;
 	unsigned int cpuset_mems_cookie;
 	unsigned int zonelist_iter_cookie;
-	int reserve_flags;
-	bool compact_first = false;
-	bool can_retry_reserves = true;
+    int reserve_flags;
+    bool compact_first = false;
+    bool can_retry_reserves = true;
+    pg_data_t *pgdat = ac->preferred_zoneref->zone->zone_pgdat;
+    bool woke_kswapd = false;
 
 	if (unlikely(nofail)) {
 		/*
@@ -4766,9 +4768,17 @@ restart:
 	}
 
 retry:
+    /* Ensure kswapd doesn't accidentally go to sleep as long as we loop */
+retry:
 	/* Ensure kswapd doesn't accidentally go to sleep as long as we loop */
 	if (alloc_flags & ALLOC_KSWAPD)
+	if (alloc_flags & ALLOC_KSWAPD) {
+		if (!woke_kswapd) {
+			atomic_inc(&pgdat->kswapd_waiters);
+			woke_kswapd = true;
+		}
 		wake_all_kswapds(order, gfp_mask, ac);
+	}
 
 	/*
 	 * The adjusted alloc_flags might result in immediate success, so try
@@ -4959,9 +4969,12 @@ nopage:
 		goto retry;
 	}
 fail:
-	warn_alloc(gfp_mask, ac->nodemask,
-			"page allocation failure: order:%u", order);
 got_pg:
+	if (woke_kswapd)
+		atomic_dec(&pgdat->kswapd_waiters);
+	if (!page)
+		warn_alloc(gfp_mask, ac->nodemask,
+				"page allocation failure: order:%u", order);
 	return page;
 }
 
